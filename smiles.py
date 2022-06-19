9c39e99d1b847951bf96c83b135a8c253412a090
Андрей хухарев <alf.master.mail@gmail.com>
2022-06-19 22:38:20 +0300

    программа рисует 50 рандомных смайлов

--- smiles.py
+++ smiles.py
@@ -23,6 +23,12 @@ def draw_smile(x, y):
     t.begin_fill()
     t.circle(10)
     t.end_fill()
+    #right eye
+    t.setpos(x+15, y+60)
+    t.fillcolor('blue')
+    t.begin_fill()
+    t.circle(10)
+    t.end_fill()    
     #roth
     t.setpos(x-25, y+40)
     t.pencolor('black')
@@ -31,6 +37,9 @@ def draw_smile(x, y):
     t.goto(x+10, y+20)
     t.goto(x+25, y+40)
     t.width(1)
-    
+for n in range(50):
+    x = random.randrange(-turtle.window_width()//2, turtle.window_width()//2)
+    y = random.randrange(-turtle.window_height()//2, turtle.window_height()//2)
+    draw_smile(x, y)
     
 
