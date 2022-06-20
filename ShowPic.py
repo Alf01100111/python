4100946ff5752d082219406ceb0382af7c9723f1
Khukharev Andrey <107508594+Alf01100111@users.noreply.github.com>
2022-06-20 13:04:46 +0300

    Update ShowPic.py
    добавил стирание шлейфа.

--- ShowPic.py
+++ ShowPic.py
@@ -7,12 +7,14 @@ colorkey = pic.get_at((0,0))
 pic.set_colorkey(colorkey)
 picx = 0
 picy = 0
+BLACK = (0, 0, 0)
 
 while keep_going:
     for event in pygame.event.get():
         keep_going = False
     picx += 1 #Передвинуть картинку
     picy += 1
+    screen.fill(BLACK)
     screen.blit(pic, (picx,picy))
     pygame.display.update()
    
