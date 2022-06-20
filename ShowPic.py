7c36368f015a1cdde777681049e889934bc443d2
Khukharev Andrey <107508594+Alf01100111@users.noreply.github.com>
2022-06-20 13:01:01 +0300

    Update ShowPic.py
    добавил движение

--- ShowPic.py
+++ ShowPic.py
@@ -1,12 +1,19 @@
 import pygame
 pygame.init()
-screen = pygame.dysplay.set_mode([800,600])
+screen = pygame.dysplay.set_mode([600,600])
 keep_going = True
 pic = picgame.image.load("CrazySmile.bmp")
+colorkey = pic.get_at((0,0))
+pic.set_colorkey(colorkey)
+picx = 0
+picy = 0
+
 while keep_going:
     for event in pygame.event.get():
         keep_going = False
-    screen.blit(pic, (100,100))
+    picx += 1 #Передвинуть картинку
+    picy += 1
+    screen.blit(pic, (picx,picy))
     pygame.display.update()
    
 pygame.quit() #Quit
