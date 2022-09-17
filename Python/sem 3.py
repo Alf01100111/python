a647562ca098953c10af1efa3017e10d23413a62
Андрей хухарев <alf.master.mail@gmail.com>
2022-09-17 11:59:17 +0300

    sem 5

---
+++ Python/sem 3.py
@@ -0,0 +1,23 @@
+# import time
+# def random ():
+#     ms = time.time()*5
+#     print(ms)
+#     b = int(ms)
+#     d = ms - b
+#     print(d)
+#     interval=int(input('введите интервал:  '))
+#     c=d*interval
+#     print('случайное число: ', int(round (c,0)))
+#     return c
+# random()
+
+
+# import time
+
+# def my_random(minn, maxx):
+#     time.sleep(0.3)
+#     return int((time.time() % 1  * (maxx - minn)) + minn)
+# #                     0.9                  7           1
+
+# for i in range(10):
+#     print(my_random(1, 9))
